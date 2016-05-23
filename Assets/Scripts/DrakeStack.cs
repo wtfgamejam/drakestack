fd3a8440ec86466efab50a5d1ca54836e5b826aa
Amesh Narsing <amesh@hololabs.org>
2016-05-23 12:37:46 -0700

    Drake drops

--- Assets/Scripts/DrakeStack.cs
+++ Assets/Scripts/DrakeStack.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class DrakeStack : MonoBehaviour {
 
+	public GameObject drakePrefab;
 	// Use this for initialization
 	void Start () {
-	
+		drakePrefab.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if (Input.GetMouseButtonDown(0)){
+			Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			GameObject drake = (GameObject)Instantiate(drakePrefab, Vector3.zero, Quaternion.identity);	
+			drake.transform.SetParent(transform, false);
+			drake.transform.localPosition = new Vector3(p.x, p.y, 0f);
+			drake.SetActive(true);
+		}
+		
 	}
 }
