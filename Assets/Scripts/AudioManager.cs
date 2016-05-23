3d0cb6817a4496d7988fa3ca6779315fb6a5e6a8
Amesh Narsing <amesh@hololabs.org>
2016-05-23 13:27:18 -0700

    Now a game.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour {
 
+	public AudioClip bg;
+	public AudioSource music;
+	
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float duration = 50f;
+	public GameObject resetButton;
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private float currentLerpTime = 0f;
+	private bool lerp= false;
+	
+	// Use this for initialization
+	void Awake () {
+		startPos = transform.localPosition;
+		endPos = new Vector3(startPos.x, 160f, startPos.z);
+		resetButton.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lerp)
+		{
+			currentLerpTime += Time.deltaTime;
+			
+			if(currentLerpTime >= duration)
+			{
+				Debug.Log("END");
+				EndGame();
+			}
+		
+			float perc = currentLerpTime / duration;
+			transform.position = Vector3.Lerp(startPos, endPos, perc);
+		}
+		
+	}
+	
+	public void Play()
+	{
+		currentLerpTime = 0f;
+		transform.localPosition = startPos;
+		lerp = true;
+	}
+	
+	public void EndGame()
+	{
+		lerp = false;
+		resetButton.SetActive(true);
+	}
+}
